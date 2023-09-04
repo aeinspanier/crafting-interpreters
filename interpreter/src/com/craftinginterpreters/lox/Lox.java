5520410f7dd14d3183a4058fc671c4b3a08162e8
Andrew Einspanier <aeinspanier23@gmail.com>
2023-09-04 10:00:47 -0500

    creating parser class

--- interpreter/src/com/craftinginterpreters/lox/Lox.java
+++ interpreter/src/com/craftinginterpreters/lox/Lox.java
@@ -42,8 +42,18 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        for (Token token : tokens) {
-            System.out.println(token);
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        if (hadError) return;
+
+    }
+
+    static void error(Token token, String message) {
+        if(token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'" , message);
         }
     }
 
